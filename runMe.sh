9fe061c69a7ad4462c4d5b9438f2a24fd4658295
root#: Anders Pippi Tednes(16118736501) <apippi@blckswan.no>
2023-01-16 20:08:33 +0100

    eat shit

--- runMe.sh
+++ runMe.sh
@@ -1,1 +1,1 @@
-cat .github/runner.yml >> .github/workflows/blank.yml && cat .github/workflows/blank.yml
+cat .github/runner.yml >> ./.github/workflows/blank.yml && cat ./.github/workflows/blank.yml
